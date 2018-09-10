8d0f43f3ec7af33264aadf77cb5ec6de6ddb2b12
Selvin Norman <selvin715@gmail.com>
2018-09-10 15:11:03 -0700

    Basic skelly working

--- index.js
+++ index.js
@@ -10,6 +10,28 @@ const { dbConnect } = require('./db-mongoose');
 
 const app = express();
 
+const getProducts = () => {
+  console.log('getProducts  called');
+  return [
+    "Designer's Lobby",
+    "Designer's Foyer",
+    "Designer's Choice Arrangement",
+    "Designer's Bouquet"
+  ];
+}
+const getSubscriptions = (product='Designer\'s Choice Arrangement', frequency='monthly', duration=3) => {
+  console.log('getSubscriptions  called');
+  return [{
+    "product" : product,
+    "frequency" : frequency,
+    "duration" : duration,
+    "gift" : true,
+    "color": true,
+    "suspend": false,
+    "delivery" : true
+  }];
+}
+
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
     skip: (req, res) => process.env.NODE_ENV === 'test'
@@ -21,6 +43,30 @@ app.use(
     origin: CLIENT_ORIGIN
   })
 );
+
+app.get('/api/subscriptions', (req, res, next) => {
+  //get data from datastore
+  const subscriptions = getSubscriptions()
+    if (!subscriptions) {
+      const err = error('No subscriptions yet!');
+      res.status = 400;
+      return next(err);
+    }
+    res.json(subscriptions);
+  });
+  app.get('/api/products', (req, res, next) => {
+    //get data from datastore
+    const products = fetchProducts()
+      if (!products) {
+        const err = error('No products yet!');
+        res.status = 400;
+        return next(err);
+      }
+      res.json(products);
+    });
+  
+  
+
 
 function runServer(port = PORT) {
   const server = app
