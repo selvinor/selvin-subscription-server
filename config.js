de414d536110cb93c09d938822966712e16ab47e
Selvin Norman <selvin715@gmail.com>
2018-09-12 16:36:28 -0700

    added jwt

--- config.js
+++ config.js
@@ -2,15 +2,12 @@
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'mongodb://dev:breaktheinternet1@ds151602.mlab.com:51602/testdb10',
   DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
+        process.env.DATABASE_URL || 'mongodb://dev:breaktheinternet1@ds151602.mlab.com:51602/testdb10',
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
-        'mongodb://localhost/thinkful-backend-test'
-  // DATABASE_URL:
-  //     process.env.DATABASE_URL || 'postgres://localhost/thinkful-backend',
-  // TEST_DATABASE_URL:
-  //     process.env.TEST_DATABASE_URL ||
-  //     'postgres://localhost/thinkful-backend-test'
+        'mongodb://localhost/thinkful-backend-test',
+  JWT_SECRET : process.env.JWT_SECRET,
+  JWT_EXPIRY : process.env.JWT_EXPIRY || '15m'
 };
--- index.js
+++ index.js
@@ -1,40 +1,114 @@
-'use strict';
+"use strict";
+require('dotenv').config();
+const express = require("express");
+const mongoose = require('mongoose');
+const cors = require("cors");
+const morgan = require("morgan");
+const passport = require('passport');
+const { router: usersRouter } = require('./users');
+const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
 
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
+mongoose.Promise = global.Promise;
 
-const { PORT, CLIENT_ORIGIN } = require('./config');
-const { dbConnect } = require('./db-mongoose');
-// const {dbConnect} = require('./db-knex');
+const { PORT, CLIENT_ORIGIN } = require("./config");
+const { dbConnect } = require("./db-mongoose");
+//const {dbConnect} = require("./db-knex");
+
+
 
 const app = express();
+// Logging
+app.use(morgan('common'));
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
 
-const getProducts = () => {
-  console.log('getProducts  called');
-  return [
-    "Designer's Lobby",
-    "Designer's Foyer",
-    "Designer's Choice Arrangement",
-    "Designer's Bouquet"
-  ];
-}
-const getSubscriptions = (product='Designer\'s Choice Arrangement', frequency='monthly', duration=3) => {
-  console.log('getSubscriptions  called');
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+app.use('/api/users/', usersRouter);
+app.use('/api/auth/', authRouter);
+
+const jwtAuth = passport.authenticate('jwt', { session: false });
+
+const getSubscriptions = () => {
+  console.log("getSubscriptions  called");
   return [{
-    "product" : product,
-    "frequency" : frequency,
-    "duration" : duration,
-    "gift" : true,
-    "color": true,
-    "suspend": false,
-    "delivery" : true
+    "userId" : 1,
+    productType : "Designer's Choice Arrangement",
+    productColor :  "color",
+    productSize :  "standard",
+    frequency : "monthly", 
+    duration :  "3",
+    gift :  true,
+    giftMessage :  null,
+    color :  true,
+    suspended :  false, 
+    delivery:  true,
+    recipients :  [
+      {
+        "firstName" : "Jane",
+        "lastName" : "User",
+        "address1" : "123 NW Maple St.",
+        "address2" : null,
+        "city" : "Portland",
+        "state" : "Oregon",
+        "zipcode" : 97204
+      },
+      {
+        "firstName" : "Jill",
+        "lastName" : "User",
+        "address1" : "222 SE Main St.",
+        "address2" : null,
+        "city" : "Portland",
+        "state" : "Oregon",
+        "zipcode" : 97201
+      },
+      {
+        "firstName" : "Jody",
+        "lastName" : "User",
+        "address1" : "17 NE Tough Rd.",
+        "address2" : null,
+        "city" : "Portland",
+        "state" : "Oregon",
+        "zipcode" : 97210
+      }
+    ]
+  },
+  {
+    userId :  2,
+    productType : "Designer's Bouquet",
+    productColor :  "color",
+    productSize :  "standard",
+    frequency : "monthly", 
+    duration :  "6",
+    gift :  true,
+    giftMessage :  null,
+    color :  true,
+    suspended :  false, 
+    delivery:  true,
+    recipients :  [
+      {
+        "firstName" : "Bar",
+        "lastName" : "Foo",
+        "address1" : "77 SE Dump Rd.",
+        "address2" : null,
+        "city" : "Portland",
+        "state" : "Oregon",
+        "zipcode" : 97215
+      }
+    ]
   }];
 }
-
 app.use(
-  morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
-    skip: (req, res) => process.env.NODE_ENV === 'test'
+  morgan(process.env.NODE_ENV === "production" ? "common" : "dev", {
+    skip: (req, res) => process.env.NODE_ENV === "test"
   })
 );
 
@@ -44,37 +118,68 @@ app.use(
   })
 );
 
-app.get('/api/subscriptions', (req, res, next) => {
+app.get("/api/subscriptions", (req, res, next) => {
   //get data from datastore
   const subscriptions = getSubscriptions()
     if (!subscriptions) {
-      const err = error('No subscriptions yet!');
+      const err = error("No subscriptions yet!");
       res.status = 400;
       return next(err);
     }
     res.json(subscriptions);
   });
-  app.get('/api/products', (req, res, next) => {
-    //get data from datastore
-    const products = fetchProducts()
-      if (!products) {
-        const err = error('No products yet!');
-        res.status = 400;
-        return next(err);
-      }
-      res.json(products);
-    });
-  
-  
 
+  app.post("/api/subscriptions", (req, res, next) => {
+    const { productType, productColor, productSize, frequency, duration, gift, color, suspended, delivery, recipients } = req.body;
+    //const userId = req.user.id;
+    //console.log("req.user", req.user);
+    /***** Never trust users - validate input *****/
+    if (!product) {
+      const err = new Error("Missing `product` in request body");
+      err.status = 400;
+      return next(err);
+    }
+  
+    // if (folderId && !mongoose.Types.ObjectId.isValid(userId)) {
+    //   const err = new Error("The `userId` is not valid");
+    //   err.status = 400;
+    //   return next(err);
+    // }
+    //console.log("recipients", recipients);
+  
+    // if (recipients) {
+    //   recipients.filter((recipient) => {
+    //     //console.log("tags", tag);
+    //     if (!mongoose.Types.ObjectId.isValid(recipient)) {
+    //       const err = new Error("The `recipients` array contains an invalid `id`");
+    //       err.status = 400;
+    //       return next(err);
+    //     }
+    //   })
+    // };
 
+  
+    const newSubscription = { productType, productColor, productSize, frequency, duration, gift, color, suspended, delivery, recipients };
+  
+    Subscription.create(newSubscription) //
+      .then(result => {
+        res
+          .location(`${req.originalUrl}/${result.id}`)
+          .status(201)
+          .json(result); //
+      })
+      .catch(err => {
+        next(err);
+      });
+  });
+  
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
       console.info(`App listening on port ${server.address().port}`);
     })
-    .on('error', err => {
-      console.error('Express failed to start');
+    .on("error", err => {
+      console.error("Express failed to start");
       console.error(err);
     });
 }
