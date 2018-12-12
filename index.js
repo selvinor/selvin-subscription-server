a000a7fc69ed354930937045d0bda0e2bbff3ab8
Selvin Norman <selvin715@gmail.com>
2018-12-12 12:43:09 -0800

    cors changes

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ app.use(cors());
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Origin', 'https://blooms-pdx.herokuapp.com');
   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
