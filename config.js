6216ac5acc57fc7ff6c14444235af2bcd43f9d90
Selvin Norman <selvin715@gmail.com>
2018-12-12 09:24:41 -0800

    added MONGODB_URI for Heroku

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL: process.env.DATABASE_URL,
+  MONGODB_URI: process.env.DATABASE_URL,
   TEST_DATABASE_URL:
   process.env.DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET,
