8ced4e8bd88df59142ba24153aed709d91166e02
Selvin Norman <selvin715@gmail.com>
2018-12-27 13:55:22 -0800

    set config to heroku

--- config.js
+++ config.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000', 
+  // CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000', 
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://blooms-pdx.herokuapp.com', 
   DATABASE_URL: process.env.DATABASE_URL,
   TEST_DATABASE_URL: process.env.DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET,
