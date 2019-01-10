f6c0e94c34a1296b0ccf98ef429b0057bd9cfb94
Selvin Norman <selvin715@gmail.com>
2019-01-10 12:11:27 -0800

    cors issue

--- index.js
+++ index.js
@@ -35,24 +35,25 @@ app.use(
     skip: (req, res) => process.env.NODE_ENV === "test"
   })
 );
-console.log("CLIENT_ORIGIN: ", CLIENT_ORIGIN);
+app.use(cors());
 //CORS
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(204);
-  }
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+//   if (req.method === 'OPTIONS') {
+//     return res.sendStatus(204);
+//   }
+//   next();
+// });
 
 
-app.use(
-  cors({
-    origin: CLIENT_ORIGIN
-  })
-);
+// app.use(
+//   cors({
+//     origin: CLIENT_ORIGIN
+//   })
+// );
 
 passport.use(localStrategy);
 passport.use(jwtStrategy);
